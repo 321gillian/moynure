864cc808c26f36510143e844fb2c0d7106b369fc
Gillian Middleton <gillianmiddleton@Gillians-MacBook-Pro.local>
2021-01-02 12:44:38 +0000

    basic hero

---
+++ components/Hero.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="Hero">
+    <div class="Hero__mainText">
+      <h1>Moynure</h1>
+      <h3>Web design</h3>
+      <p class="Hero__subtitle py-2 mt-5">
+        Simple, beautiful, easy-to-use websites
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+.Hero {
+  background: url("~assets/img/bg-large.png");
+  background-color: rgba($red-3, 0.8);
+  background-blend-mode: multiply;
+  width: 100%;
+  height: 650px;
+  display: flex;
+  align-items: center;
+
+  .Hero__mainText {
+    text-align: right;
+    flex: 1 0 auto;
+    color: white;
+
+    h1,
+    h3 {
+      margin-right: 250px;
+    }
+
+    h3 {
+      text-transform: uppercase;
+    }
+
+    .Hero__subtitle {
+      color: $black-2;
+      margin-top: 60px;
+      background: white;
+      width: 100%;
+      padding-right: 250px;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,39 @@
 <template>
-  <div class="container">
+  <div class="main">
     <Header />
+    <Hero />
   </div>
 </template>
 
 <script>
-import Header from '../components/Header.vue'
+import Header from "../components/Header.vue";
+import Hero from "../components/Hero.vue";
 export default {
   components: {
     Header,
+    Hero,
   },
-}
+};
 </script>
 
 <style>
 .container {
-  margin: 0 auto;
+  /* margin: 0 auto;
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
+  text-align: center; */
+  position: relative;
+}
+
+.main {
+  width: 100%;
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
